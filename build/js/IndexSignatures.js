908da98a581bdaedee6e2170cd0ac24f0081b68b
Farouk-ayo <mustaphafarouk41@gmail.com>
2023-08-26 21:59:30 +0100

    index signatures

---
+++ build/js/IndexSignatures.js
@@ -0,0 +1,9 @@
+"use strict";
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+// dynamically getting the data
---
+++ src/IndexSignatures.ts
@@ -0,0 +1,16 @@
+interface TransactionObj {
+  Pizza: number;
+  Books: number;
+  Job: number;
+}
+
+const todaysTransactions: TransactionObj = {
+  Pizza: -10,
+  Books: -5,
+  Job: 50,
+};
+
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+
+// dynamically getting the data
